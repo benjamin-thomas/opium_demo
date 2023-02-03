45eaa2739110002e1e03903c2b64ace36ac01394
Benjamin <benjamin.guy.thomas@gmail.com>
2023-02-03 06:42:04 +0100

    Add DB setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Opium demo
+
+## Dev setup
+
+Run one of those commands:
+
+```bash
+./manage/dev/run # resets the DB on app restarts
+RESET=0 ./manage/dev/run
+```
---
+++ manage/sql/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE person
+  ( id INTEGER PRIMARY KEY AUTOINCREMENT
+  , fname TEXT NOT NULL
+  , lname TEXT NOT NULL
+  , age INT NOT NULL
+  );
+
+INSERT INTO person (fname, lname, age)
+VALUES ('John', 'Doe', 99)
+     , ('Jane', 'Doe', 98)
+     ;
